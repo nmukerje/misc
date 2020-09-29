2cb77982b342d6c0a25b191ca90b0782953a7c13
nmukerje <nmukerje@gmail.com>
2020-09-29 10:49:35 -0700

    Update emr_sqoop_bootstrap.sh

--- Step_Functions_EMR/emr_sqoop_bootstrap.sh
+++ Step_Functions_EMR/emr_sqoop_bootstrap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# adds S3 support to SQOOP 1.4.7
 wget https://repo1.maven.org/maven2/org/kitesdk/kite-data-s3/1.1.0/kite-data-s3-1.1.0.jar
 sudo cp kite-data-s3-1.1.0.jar /usr/lib/sqoop/lib/
 sudo chmod 755 kite-data-s3-1.1.0.jar
