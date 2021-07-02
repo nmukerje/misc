b73d2fae9586f625537983541f61d9d4ddbc953c
nmukerje <nmukerje@gmail.com>
2021-07-01 18:28:54 -0700

    Add files via upload

---
+++ EMR_EBS_GP3/switch_gp2_to_gp3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+gp2_volumes=$(aws ec2 describe-volumes --filters Name=attachment.instance-id,Values=`curl -s http://169.254.169.254/latest/meta-data/instance-id` | jq -r '.Volumes[] | select(.VolumeType=="gp2").VolumeId')
+
+for v in $gp2_volumes
+do
+	echo "Switching $v from GP2 to GP3."
+	aws ec2 modify-volume --volume-type gp3 --volume-id $v
+done
