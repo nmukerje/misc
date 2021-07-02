93c248cbabac9a99202c09dd6cb4d2a9cc5c6f56
nmukerje <nmukerje@gmail.com>
2021-07-01 18:34:31 -0700

    Update switch_gp2_to_gp3.sh

--- EMR_EBS_GP3/switch_gp2_to_gp3.sh
+++ EMR_EBS_GP3/switch_gp2_to_gp3.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# get GP2 volumes
+# get gp2 volumes
 gp2_volumes=$(aws ec2 describe-volumes --filters Name=attachment.instance-id,Values=`curl -s http://169.254.169.254/latest/meta-data/instance-id` | jq -r '.Volumes[] | select(.VolumeType=="gp2").VolumeId')
 
 for v in $gp2_volumes
 do
 	echo "Switching $v from GP2 to GP3."
-	# Switch them to GP3
+	# Switch them to gp3
 	aws ec2 modify-volume --volume-type gp3 --volume-id $v
 done
